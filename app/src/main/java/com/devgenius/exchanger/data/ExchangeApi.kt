764e67603d78b2a4005c67afc223d7a50172eb89
evgeniagrigorovic <evgeniya_8817@mail.ru>
2022-03-16 18:55:16 +0300

    Fix query

--- app/src/main/java/com/devgenius/exchanger/data/ExchangeApi.kt
+++ app/src/main/java/com/devgenius/exchanger/data/ExchangeApi.kt
@@ -1,17 +1,24 @@
 package com.devgenius.exchanger.data
 
+import com.devgenius.exchanger.BuildConfig
 import com.devgenius.exchanger.data.entity.CurrencyDTO
+import com.devgenius.exchangerdi.di.NetworkModule
+import dagger.Module
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 /**
  * Api ретрофита для получения валют
  */
+@Module(includes = [NetworkModule::class])
 interface ExchangeApi {
 
     /**
      * Создает get зарпос
      */
-    @GET
-    fun getCurrency(): Response<CurrencyDTO>
+    @GET("latest")
+    suspend fun getCurrency(@Query(BuildConfig.API_KEY_LABEL) string: String): Response<CurrencyDTO>
 }
