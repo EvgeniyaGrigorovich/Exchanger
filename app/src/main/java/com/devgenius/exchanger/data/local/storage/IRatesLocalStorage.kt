3a83c6076cbd059485224c04d248691d8e65477a
evgeniagrigorovic <evgeniya_8817@mail.ru>
2022-03-19 20:28:04 +0300

    Created local storage

---
+++ app/src/main/java/com/devgenius/exchanger/data/local/storage/IRatesLocalStorage.kt
@@ -0,0 +1,25 @@
+package com.devgenius.exchanger.data.local.storage
+
+import com.devgenius.exchanger.data.local.db.dbmodel.RateDbModel
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Локал сторедж для добавления и получения избранных валют
+ *
+ * @author Evgeniia Grigorovich
+ */
+interface IRatesLocalStorage {
+
+    /**
+     * Метод для сохранения валюты в избранное
+     *
+     * @param rate Валюта для сохранения
+     */
+    suspend fun saveFavouriteRate(rate: RateDbModel)
+
+    /**
+     * Метод получения списка избранных валют
+     */
+    suspend fun getFavouritesRates(): Flow<List<RateDbModel>>
+
+}
---
+++ app/src/main/java/com/devgenius/exchanger/data/local/storage/RatesLocalStorage.kt
@@ -0,0 +1,25 @@
+package com.devgenius.exchanger.data.local.storage
+
+import com.devgenius.exchanger.data.local.db.RatesDao
+import com.devgenius.exchanger.data.local.db.dbmodel.RateDbModel
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Реализация [IRatesLocalStorage]
+ *
+ * @param ratesDao класс для работы с базой данных
+ *
+ * @author Evgeniia Grigorovich
+ */
+class RatesLocalStorage(
+    private val ratesDao: RatesDao
+): IRatesLocalStorage {
+
+    override suspend fun saveFavouriteRate(rate: RateDbModel) {
+       ratesDao.saveRate(rate = rate)
+    }
+
+    override suspend fun getFavouritesRates(): Flow<List<RateDbModel>> {
+        return ratesDao.getAllRates()
+    }
+}
