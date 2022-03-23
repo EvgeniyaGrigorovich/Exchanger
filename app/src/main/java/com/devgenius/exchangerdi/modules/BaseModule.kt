9ddc0afe1d0ba98f2df8f1a839c272ff03e3f153
evgeniagrigorovic <evgeniya_8817@mail.ru>
2022-03-23 15:01:18 +0300

    Some ui fix

--- app/src/main/java/com/devgenius/exchangerdi/modules/BaseModule.kt
+++ app/src/main/java/com/devgenius/exchangerdi/modules/BaseModule.kt
@@ -1,6 +1,6 @@
 package com.devgenius.exchangerdi.modules
 
-import com.devgenius.exchanger.presentation.MainScreenViewStateReducer
+import com.devgenius.exchanger.presentation.reducer.MainScreenViewStateReducer
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
